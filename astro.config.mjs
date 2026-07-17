d931e38b0c228c35c64b778585c12e436d0a3bf5
slain.sh <zachquita@gmail.com>
2026-07-17 09:15:04 +0800

    hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,51 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	fonts: [
+		{
+			provider: fontProviders.local(),
+			name: "Instrument Serif",
+			cssVariable: "--font-instrument-serif",
+			options: {
+				variants: [
+					{
+						src: ["./src/assets/fonts/InstrumentSerif-Regular.woff2"],
+						weight: "normal",
+						style: "normal",
+					},
+					{
+						src: ["./src/assets/fonts/InstrumentSerif-Italic.woff2"],
+						weight: "normal",
+						style: "italic",
+					},
+				],
+			},
+			fallbacks: ["Times New Roman", "Times", "serif"],
+		},
+		{
+			provider: fontProviders.fontshare(),
+			name: "Switzer",
+			cssVariable: "--font-switzer",
+			options: {
+				variants: [
+					{
+						weight: ["200 600"],
+						style: "normal",
+					},
+					{
+						weight: ["200 600"],
+						style: "italic",
+					},
+				],
+			},
+			fallbacks: [
+				"system-ui",
+				"-apple-system",
+				"BlinkMacSystemFont",
+				"sans-serif",
+			],
+		},
+	],
+});
